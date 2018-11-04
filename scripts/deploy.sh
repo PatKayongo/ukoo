babcc69b6db0a30a896a7f7f96429893bc876f47
Patrick Kayongo <pat.kayongo@gmail.com>
2018-11-04 22:03:59 +0200

    Add ssh password environment variable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 mkdir -p ~/.ssh
 echo "$UKOO_SSH_KEY" > ~/.ssh/id_rsa_ukoo_key
 sudo chmod 600 ~/.ssh/id_rsa_ukoo_key
+sshpass -p "$UKOO_SSH_PASSWORD" ssh-add ~/.ssh/id_rsa_ukoo_key
 
 npm install -g pm2
 pm2 deploy production update
