f58160fb8921b45fe6457115dcea0c5e0124bcbd
Patrick Kayongo <pat.kayongo@gmail.com>
2018-11-04 22:20:07 +0200

    Use ssh add to add the key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,7 @@
 mkdir -p ~/.ssh
 echo "$UKOO_SSH_KEY" > ~/.ssh/id_rsa_ukoo_key
 sudo chmod 600 ~/.ssh/id_rsa_ukoo_key
-sudo apt-get install sshpass
-sshpass -p "$UKOO_SSH_PASSWORD" ssh-add ~/.ssh/id_rsa_ukoo_key
+sudo ssh-add ~/.ssh/id_rsa_ukoo_key
 
 npm install -g pm2
 pm2 deploy production update
