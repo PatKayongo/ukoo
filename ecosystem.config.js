ad8063629813d10e1ef42d2e352d5ea57bdcb91b
Patrick Kayongo <pat.kayongo@gmail.com>
2018-11-04 19:57:21 +0200

    Add pm2 file

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps: [{
+    name: 'Ukoo',
+    script: 'src/infrastructure/web/index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+    },
+    env_production: {
+      NODE_ENV: 'production',
+    },
+  }],
+
+  deploy: {
+    production: {
+      key: '~/.ssh/id_rsa_ukoo',
+      user: 'ukoo',
+      host: 'ukoo.patkayongo.africa',
+      ref: 'origin/master',
+      repo: 'https://github.com/PatKayongo/ukoo.git',
+      path: '/home/ukoo/ukoo',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
