c4f1f8f35eccb496e9b3ff56c371f03cfbe6b740
Patrick Kayongo <pat.kayongo@gmail.com>
2018-11-04 18:14:03 +0200

    Added ESLint

--- src/infrastructure/web/index.js
+++ src/infrastructure/web/index.js
@@ -1,7 +1,11 @@
+
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const port = process.env.APPLICATION_PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Ukoo listening on port ${port}!`));
